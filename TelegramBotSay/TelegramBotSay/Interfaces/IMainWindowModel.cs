926fa22c6a2dfd0b73012658599288452bf0eef9
alexey.anipchenko <alex.anipchenko@gmail.com>
2019-02-02 12:28:25 +0200

    Add interfaces

---
+++ TelegramBotSay/TelegramBotSay/Interfaces/IMainWindowModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TelegramBotSay.Interfaces
+{
+    public interface IMainWindowModel
+    {
+        DateTime NexTimeSending { get; set; }
+
+        string MessageToSend { get; set; }
+
+        string TextInEdit { get; set; }
+
+        string TextRrecpient { get; set; }
+
+        RelayCommand CommandSaveClick { get; set; }
+
+        RelayCommand CommandSendNowClick { get; set; }
+
+        RelayCommand CommandChangeRecepientClick { get; set; }
+    }
+}
---
+++ TelegramBotSay/TelegramBotSay/Interfaces/IWindowInputDialogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TelegramBotSay.Interfaces
+{
+    public interface IWindowInputDialogModel
+    {
+        string TextEdit { get; set; }
+
+        string TextPlaceholderText { get; }
+
+        string TextButtonText { get; }
+
+        RelayCommand CommandCloseWindowClick { get; set; }
+    }
+}
